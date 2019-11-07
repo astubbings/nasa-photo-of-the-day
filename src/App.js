71056535add145042933d6fe8d4189e97fbc45cc
astubbings <53932400+astubbings@users.noreply.github.com>
2019-11-07 14:39:55 -0700

    commit first styled-component on App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import NasaData from "./NasaData";
 
+import styled from 'styled-components'
 
 // the following is the BASIC data request to see data structure and console log it
 // import axios from "axios";
@@ -18,13 +19,21 @@ import NasaData from "./NasaData";
 
  // END the BASIC data request  
 
+const Instructions = styled.p`
+    color: dodgerblue;
+    
+    `
+
+
 function App() {
   return (
     <div className="App">
-      <p>
+      <Instructions>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role='img' aria-label='rocket emoji'> 🚀</span>!
-      </p>
+      </Instructions>
+
+
       <NasaData/>
     </div>
   );
